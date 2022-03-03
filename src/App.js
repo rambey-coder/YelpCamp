dcf5125dd6eabaaad249adf232607fa200b3539e
rambey-coder <olayiwolaagboolaakanji@gmail.com>
2022-03-03 17:48:31 +0100

    remove a warning in App js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './components/Home';
 
 function App() {
