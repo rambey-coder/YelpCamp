c42bb263cb68c87fdc0db6bc0ff0ac0cad744628
rambey-coder <olayiwolaagboolaakanji@gmail.com>
2022-03-04 09:08:31 +0100

    landing page now working perfectly

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,75 +1,85 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Home.css';
 import { Link } from 'react-router-dom'
+import PuffLoader from "react-spinners/PuffLoader";
+
 
 const Home = () => {
+    const  [loading, setLoading] = useState(false)
+
+    useEffect (() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 6000)
+    }, [])
+
   return (
     <div className="home-general-container">
+        {
+            loading ? 
 
-        <div className="tablet-logo-container">
-                <img src="./Logo.svg" alt="yelpcamp logo"  className='logo-tab'/>
+            <div className="loaders">
+                <PuffLoader
+                className= 'loader'
+                css=''
+                size={50}
+                color={"#000"}
+                loading={loading}
+            />
             </div>
+            :
 
-        <div className="home-container">
-        {/* left container */}
-        <div className='left-container'>
+            <>
+            <div className="tab-logo-container">
+                      <img src="./Logo.svg" alt="camp-logo" className='camp-logo' />
+                  </div><div className="sub-container">
+                          <div className="left-container">
+                              <div className="logo-container">
+                                  <img src="./Logo.svg" alt="camp-logo" />
+                              </div>
 
-            <div className="cover">
-            <div className="logo-container">
-                <img src="./Logo.svg" alt="yelpcamp logo"  />
-            </div>
+                              <div className="content-container">
+                                  <h1>Explore the best camps on Earth.</h1>
+                                  <p>Yelp camp is a curated list of the best camping spots on Earth.Unlimited and unibaised reviews </p>
 
-            <div className="contents">
-                <h1>Explore the best camps on Earth.</h1>
+                                  <div className="checks">
+                                      <img src="./Checkmark.svg" alt="checkmark" className='checkmark' />
+                                      <p>Add your own camp suggestions</p>
+                                  </div>
 
-                <p>YelpCamp is a curated list of the best camping spots on Earth. Unfiltered and unbiased reviews.</p>
+                                  <div className="checks">
+                                      <img src="./Checkmark.svg" alt="checkmark" className='checkmark' />
+                                      <p>Leave reviews and experiences</p>
+                                  </div>
 
-                <div className="checks">
+                                  <div className="checks">
+                                      <img src="./Checkmark.svg" alt="checkmark" className='checkmark' />
+                                      <p>See locations to all camps</p>
+                                  </div>
 
-                <img src="./Checkmark.svg" alt="" className='checkmark'/>
-                <p> Add your own camp suggestions.</p>
-                
-                </div>
-                <div className="checks">
+                                  <Link to='search-page' className='home-btn'>
+                                      View Campgrounds
+                                  </Link>
 
-                <img src="./Checkmark.svg" alt="" className='checkmark'/>
-                <p> Leave reviews and experiences.</p>
+                                  <p>Partnered with:</p>
+                                  <div className="patners">
+                                      <img src="./Airbnb.svg" alt="Airbnb logo" />
+                                      <img src="./Booking.svg" alt="Booking.svg" />
+                                      <img src="./Plum Guide.svg" alt="Plum Guide" />
+                                  </div>
+                              </div>
+                          </div>
 
-                </div>
-                <div className="checks">
+                          <div className="right-container">
+                              <img src="./Hero-Image.jpg" alt="hero" className='hero-image' />
 
-                <img src="./Checkmark.svg" alt="" className='checkmark'/>
-                <p>See locations for all camps.</p>
+                              <img src="./Section Image.jpg" alt="section" className='section-image' />
+                          </div>
+                      </div>
+                      </>
+        }
 
-                </div>
-
-                <div>
-                <Link to='/searchpage' className='home-btn'>
-                    View Campgrounds
-                </Link>
-                </div>
-
-                <p>Partnered with:</p>
-
-                <div className="patner-logo">
-                    <img src="./Airbnb.svg" alt="Airbnb Logo" />
-
-                    <img src="./Booking.svg" alt="Booking" />
-
-                    <img src="./Plum Guide.svg" alt="Plum Guide" />
-                </div>
-               
-            </div>
-            </div>
-
-        </div>
-
-        {/* right container */}
-        <div className="right-container">
-            {/* <img src="./Hero-Image.jpg" alt="camp-image" /> */}
-            <div className="image-container"></div>
-        </div>
-    </div>
     </div>
   )
 }
